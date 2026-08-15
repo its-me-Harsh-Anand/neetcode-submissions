4c6dace6fc540523e834d3d41a4fd27785e7155f
Harsh Anand <75748840+its-me-Harsh-Anand@users.noreply.github.com>
2026-08-15 15:11:11 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        String encoded = "";
+        for(String el : strs){
+            Integer size = el.length();
+            encoded += size + "$" + el;
+        }
+        return encoded;
+    }
+
+    public List<String> decode(String str) {
+        List<String> list = new ArrayList<String>();
+        int i = 0;
+        while(i<str.length()){
+            if (Character.isDigit(str.charAt(i))) {
+                int num = 0;
+
+                while (i < str.length() && Character.isDigit(str.charAt(i))) {
+                    num = num * 10 + (str.charAt(i) - '0');
+                    i++;
+                }
+                i++; // to surpass $
+                // String temp = "";
+                // int k = i;
+                // for(; k<num+i; k++){
+                //     temp += str.charAt(k);
+                // }
+
+                String temp = str.substring(i, num+i);
+                
+                list.add(temp);
+                i+=num;
+            }
+        }
+        return list;
+    }
+}
